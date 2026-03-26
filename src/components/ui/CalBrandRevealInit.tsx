b9e84ed7263b1333662a83c9b926ed033d5fa368
Shehper Sameer Ahmed <info@khbrokers.com>
2026-03-26 21:16:33 +0500

    Fixed

--- src/components/ui/CalBrandRevealInit.tsx
+++ src/components/ui/CalBrandRevealInit.tsx
@@ -14,6 +14,7 @@ export function CalBrandRevealInit() {
         layout: "month_view",
         cssVarsPerTheme: {
           light: { "cal-brand": "#ebdefc" },
+          dark: { "cal-brand": "#ebdefc" },
         },
       });
     })();
